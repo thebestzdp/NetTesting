33831eb9dc6c05f0da9ffa5a9f7adc4e9577cd8e
thebestzdp <thebestzdp@gmail.com>
2022-01-17 14:59:53 +0800

    添加一个chttp方法进行http请求测试

--- NetTesting/main.cpp
+++ NetTesting/main.cpp
@@ -7,9 +7,9 @@
 
 #include <iostream>
 #include "NetTesting.h"
+#include "http.h"
 
 #define PING_RES_BUFFER_LEN 1024
-
 int main(int argc, const char * argv[]) {
     
     char res_buffer[PING_RES_BUFFER_LEN];
@@ -17,8 +17,20 @@ int main(int argc, const char * argv[]) {
     int res = ping_result(domain, 2, res_buffer, PING_RES_BUFFER_LEN);
     if(!res)
         printf("PINGER: %s \n",res_buffer);
+    ///
+    ft_http_client_t* http = 0;
+    ft_http_init();
+    http = ft_http_new();
+    const char* body = 0;
+    body = ft_http_sync_request(http, "https://www.google.com", M_GET);
+    printf("ft_http get:%s\n", body);
+    if (http) {
+        ft_http_destroy(http);
+    }
+    ft_http_deinit();
+    ///
     
-    curl_connect_http("https://www.google.com");
+    curl_connect_http(domain);
     trace_route(domain);
     
     return 0;
